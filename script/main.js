c6915d38fafa69091ce1930c08b251cfe0e362bb
mludwig99 <michalludwig99@gmail.com>
2022-06-10 13:20:13 +0200

    41-sprawdzanie-liczby-2

---
+++ script/main.js
@@ -0,0 +1,9 @@
+function result(nr1, nr2) {
+    if (nr1 + nr2 <= 20 || nr1 == 20 || nr2 == 20) {
+        console.log(Boolean(true));
+    } else {
+        console.log(Boolean(false))
+    }
+}
+result(19, 5);
+result(1,7)
