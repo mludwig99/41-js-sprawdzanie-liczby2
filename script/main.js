a6562c27cb9798f79b510c7c800006b47913b782
mludwig99 <michalludwig99@gmail.com>
2022-06-15 11:12:10 +0200

    41-poprawione-liczby

--- script/main.js
+++ script/main.js
@@ -1,9 +1,10 @@
 function result(nr1, nr2) {
-    if (nr1 + nr2 <= 20 || nr1 == 20 || nr2 == 20) {
-        console.log(Boolean(true));
+    if (nr1 + nr2 <= 20 || nr1 === 20 || nr2 === 20) {
+        return true;
     } else {
-        console.log(Boolean(false))
+        return false;
     }
 }
-result(19, 5);
-result(1,7)
+console.log(result(19, 5));
+console.log(result(1, 7));
+console.log(result(20, 7));
